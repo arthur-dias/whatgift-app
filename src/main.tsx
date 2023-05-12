2af4809f64089ec16e8ccc8f03db9fffd24e0d6d
Arthur Dias <dias.arthurr@gmail.com>
2023-05-12 10:24:55 -0300

    added global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './styles/_global.css'
+import './styles/_reset.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>

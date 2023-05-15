a568f624339f1f2e466cf7ec9404bf102ae6c336
Arthur Dias <dias.arthurr@gmail.com>
2023-05-15 10:17:44 -0300

    add header tests

--- src/components/Header/Header.cy.tsx
+++ src/components/Header/Header.cy.tsx
@@ -1,7 +1,12 @@
 import Header from './Header'
 
 describe('<Header />', () => {
-  it('renders', () => {
+  it('Renders', () => {
     cy.mount(<Header title='Qual presente?' />)
+  })
+
+  it('Uses custom text for the title label', () => {
+    cy.mount(<Header title='Qual presente?' />)
+    cy.get('header').should('contains.text', 'Qual presente?')
   })
 })

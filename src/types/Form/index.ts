ed87107a8fa92bfc08fb8b0aed7835d74748f9ff
Arthur Dias <dias.arthurr@gmail.com>
2023-05-23 10:19:41 -0300

    added form types

---
+++ src/types/Form/index.ts
@@ -0,0 +1,20 @@
+export type PeopleOption = {
+  value: string
+  label: string
+}
+
+export type AgeOption = {
+  value: string
+  label: string
+}
+
+export type HobbiesOption = {
+  value: string
+  label: string
+}
+
+export type SelectedOptions = {
+  people: string
+  age: string
+  hobbies: string
+}

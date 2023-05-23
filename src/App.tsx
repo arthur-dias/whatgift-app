b2c4817ce7d92949acddafc504722d38b3156138
Arthur Dias <dias.arthurr@gmail.com>
2023-05-23 16:09:07 -0300

    added form component logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Form from './components/Form/Form'
 import Header from './components/Header/Header'
 import Presentation from './components/Presentation/Presentation'
 
@@ -9,6 +10,7 @@ function App() {
         title='Ideias de presentes de uma forma fácil e rápida!'
         text='Encontre o presente perfeito para seu amigx, parente ou colega usando o assistente GPT. Essa ferramenta facilita sua busca utilizando o modelo do GPT com o conteúdo de toda a internet! Você nunca mais irá bater cabeça para encontrar um presente de aniversário!'
       />
+      <Form />
     </>
   )
 }
---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,94 @@
+import { useState } from 'react'
+import { HobbiesOption, PeopleOption, SelectedOptions } from '../../types/form'
+import { FormData } from '../../data/formData'
+const Form = () => {
+  const [resultMessage, setResultMessage] = useState<string | null>(null)
+  const [selectedOptions, setSelectedOptions] = useState<SelectedOptions>({
+    people: '',
+    age: '',
+    hobbies: '',
+  })
+
+  const peopleOptions: PeopleOption[] = FormData.peopleOptions
+  const hobbiesOptions: HobbiesOption[] = FormData.hobbiesOptions
+
+  const handleOptionChange = (
+    category: keyof SelectedOptions,
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSelectedOptions((prevOptions) => ({
+      ...prevOptions,
+      [category]: event.target.value,
+    }))
+  }
+
+  const handleSubmit = async () => {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        message: `Me dê cinco ideias de presente para meu/minha ${selectedOptions.people} que tem ${selectedOptions.age} anos e tem como hobbies ${selectedOptions.hobbies}`,
+      }),
+    }
+
+    try {
+      const response = await fetch(
+        'https://api-whatgift.onrender.com/ask',
+        options
+      )
+      const data = await response.json()
+      setResultMessage(data.choices[0].message.content)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <div>
+      <label htmlFor='people-select'>Select someone:</label>
+      <select
+        id='people-select'
+        value={selectedOptions.people}
+        onChange={(event) => handleOptionChange('people', event)}>
+        <option value=''>-- Select --</option>
+        {peopleOptions.map((people) => (
+          <option key={people.value} value={people.value}>
+            {people.label}
+          </option>
+        ))}
+      </select>
+      <label htmlFor='age-select'>Select age:</label>
+      <select
+        id='age-select'
+        value={selectedOptions.age}
+        onChange={(event) => handleOptionChange('age', event)}>
+        <option value=''>-- Select --</option>
+        {Array.from({ length: 111 }, (_, index) => (
+          <option key={index} value={index}>
+            {index}
+          </option>
+        ))}
+      </select>
+      <label htmlFor='hobbies-select'>Select hobbies:</label>
+      <select
+        id='hobbies-select'
+        value={selectedOptions.hobbies}
+        onChange={(event) => handleOptionChange('hobbies', event)}>
+        <option value=''>-- Select --</option>
+        {hobbiesOptions.map((hobbie) => (
+          <option key={hobbie.value} value={hobbie.value}>
+            {hobbie.label}
+          </option>
+        ))}
+      </select>
+
+      <button onClick={handleSubmit}>Submit</button>
+
+      {resultMessage ? <p>{resultMessage}</p> : null}
+    </div>
+  )
+}
+
+export default Form

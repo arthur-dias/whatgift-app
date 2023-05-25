142a8762de94756675e0c2f7fa82efb41893a1ef
Arthur Dias <dias.arthurr@gmail.com>
2023-05-25 17:02:08 -0300

    text change

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,9 @@ function App() {
     <>
       <Header title='Qual presente?' />
       <Presentation
-        title='Ideias de presentes de uma forma fácil e rápida!'
-        text='Encontre o presente perfeito para seu amigx, parente ou colega usando o assistente GPT. Essa ferramenta facilita sua busca utilizando o modelo do GPT com o conteúdo de toda a internet! Você nunca mais irá bater cabeça para encontrar um presente de aniversário!'
+        title='
+Encontre ideias de presentes únicos usando o poder do GPT.'
+        text='Encontre o presente perfeito para seu amigo, família ou colega usando o assistente GPT. Essa ferramenta permite que você pesquise facilmente por presentes utilizando o modelo GPT, que é enriquecido com conteúdo de toda a internet! Você nunca mais precisará se estressar para encontrar um presente de aniversário!'
       />
       <Form />
     </>

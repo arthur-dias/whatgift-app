1015156b95ff6cb834b813557b965b4c1969d866
Arthur Dias <dias.arthurr@gmail.com>
2023-05-26 11:02:42 -0300

    text changes

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ function App() {
         text='Encontre o presente perfeito para seu amigo, família ou colega usando o assistente GPT. Essa ferramenta permite que você pesquise facilmente por presentes utilizando o modelo GPT, que é enriquecido com conteúdo de toda a internet! Você nunca mais precisará se estressar para encontrar um presente de aniversário!'
       />
       <Form />
-      <Cards />
-      <Footer text='Então, da próxima vez que você estiver com dificuldades para encontrar o presente perfeito, recorra a essa ferramenta para obter recomendações personalizadas de presentes que sejam adequadas aos interesses e preferências do destinatário. Nossas recomendações de presentes, impulsionadas pela inteligência artificial ajudarão você a encontrar um presente que certamente causará uma impressão duradoura e criar uma experiência memorável para seu ente querido.' />
+      <Cards text='Então, da próxima vez que você estiver com dificuldades para encontrar o presente perfeito, recorra a essa ferramenta para obter recomendações personalizadas de presentes que sejam adequadas aos interesses e preferências do destinatário. Nossas recomendações de presentes, impulsionadas pela inteligência artificial ajudarão você a encontrar um presente que certamente causará uma impressão duradoura e criar uma experiência memorável para seu ente querido.' />
+      <Footer text='Feito por Arthur' />
     </>
   )
 }

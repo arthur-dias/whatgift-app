cf8ad68b01ddedd2eea034baf1f65c58951cad99
Arthur Dias <dias.arthurr@gmail.com>
2023-05-26 11:02:11 -0300

    finished footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,19 @@
+import Github from '../../assets/github.svg'
+import styles from './Footer.module.css'
+
 interface FooterProps {
   text: string
 }
 const Footer = ({ text }: FooterProps) => {
+  const currentYear = new Date().getFullYear()
+
   return (
-    <div>
-      <p>{text}</p>
+    <div className={styles.footer}>
+      <p className={styles.footer__text}>{text}</p>
+      <a href='https://github.com/arthur-dias' target='_blank' rel='noreferrer'>
+        <img src={Github} alt='ícone' className={styles.footer__icon} />
+      </a>
+      <p className={styles.footer__text}>{currentYear}</p>
     </div>
   )
 }
